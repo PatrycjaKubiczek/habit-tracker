140b180b2890a07340cd813905421afd63091ecf
patrycjakubiczek <p.kubiczek@gmail.com>
2019-06-13 22:07:35 +0200

    Updates

---
+++ precache-manifest.bc48db4b96463fb433f6f4e609ec06a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5e9adc78cde9da44444cde0ead3e9aa",
+    "url": "/habit-tracker-v1/index.html"
+  },
+  {
+    "revision": "61b4d13cb7fe61a04b56",
+    "url": "/habit-tracker-v1/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "e1efeeed7976dfe47a0f",
+    "url": "/habit-tracker-v1/static/css/main.c6ea8744.chunk.css"
+  },
+  {
+    "revision": "61b4d13cb7fe61a04b56",
+    "url": "/habit-tracker-v1/static/js/2.82a673c0.chunk.js"
+  },
+  {
+    "revision": "e1efeeed7976dfe47a0f",
+    "url": "/habit-tracker-v1/static/js/main.715466e4.chunk.js"
+  },
+  {
+    "revision": "a46fcd0bde4ad7b3a745",
+    "url": "/habit-tracker-v1/static/js/runtime~main.2bcf9a4e.js"
+  }
+]);
